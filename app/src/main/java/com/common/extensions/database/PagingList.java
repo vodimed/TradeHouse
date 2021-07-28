9a57c1b5dd8505fde43d0fcc1a8aaeb00db8abdf
vodimed <dem@dxdy.ru>
2021-07-28 08:38:29 +0300

    Форма редактирования накладных InvoiceActivity

--- app/src/main/java/com/common/extensions/database/PagingList.java
+++ app/src/main/java/com/common/extensions/database/PagingList.java
@@ -12,18 +12,23 @@ import java.util.ArrayList;
 public class PagingList<Value> extends AbstractList<Value> {
     private final static int pagesize = 20;
     private final LimitOffsetDataSource<Value> source;
+    private final int count;
+    private int size;
     private final SparseArray<Value> cache = new SparseArray<Value>(pagesize);
-    private final int size;
+    private final SparseArray<Value> update = new SparseArray<Value>(pagesize);
 
     @SuppressLint("RestrictedApi")
     public PagingList(DataSource.Factory<Integer, Value> factory) {
         source = (LimitOffsetDataSource<Value>) factory.create();
-        size = source.countItems();
+        size = count = source.countItems();
     }
 
     @Override
     public Value get(int index) {
-        Value cached = cache.get(index);
+        Value cached = update.get(index);
+
+        if (cached == null)
+            cached = cache.get(index);
 
         if (cached == null) {
             if (cache.size() > 100 * pagesize) cache.clear();
@@ -41,7 +46,48 @@ public class PagingList<Value> extends AbstractList<Value> {
     }
 
     @Override
+    public Value set(int index, Value element) {
+        if (index < size) {
+            final Value previous = update.get(index);
+            update.put(index, element);
+            return previous;
+        } else if (index == size) {
+            size++;
+            update.put(index, element);
+            return null;
+        } else {
+            return super.set(index, element);
+        }
+    }
+
+    @Override
+    public boolean add(Value element) {
+        set(size, element);
+        return true;
+    }
+
+    @Override
+    public Value remove(int index) {
+        if (index < size) {
+            final Value previous = update.get(index);
+            update.delete(index);
+            return previous;
+        } else {
+            return super.remove(index);
+        }
+    }
+
+    @Override
     public int size() {
         return size;
+    }
+
+    void commit() {
+
+    }
+
+    void rollback() {
+        update.clear();
+        size = count;
     }
 }
--- app/src/main/java/com/expertek/tradehouse/InvoiceActivity.java
+++ app/src/main/java/com/expertek/tradehouse/InvoiceActivity.java
@@ -2,12 +2,70 @@ package com.expertek.tradehouse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.expertek.tradehouse.documents.entity.line;
 
 public class InvoiceActivity extends Activity {
+    private line line = null;
+    private Button buttonOk = null;
+    private Button buttonCancel = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.invoice_activity);
+
+        // Retrieve Activity parameters
+        line = (line) getIntent().getSerializableExtra(line.class.getName());
+
+        final TextView labelName = findViewById(R.id.labelName);
+        labelName.setText(line.GoodsName);
+
+        final EditText editPrice = findViewById(R.id.editPrice);
+        editPrice.setText(String.valueOf(line.Price));
+
+        final EditText editAmountDoc = findViewById(R.id.editAmountDoc);
+        editAmountDoc.setText(String.valueOf(line.DocQnty));
+
+        final EditText editAmountFact = findViewById(R.id.editAmountFact);
+        editAmountFact.setText(String.valueOf(line.FactQnty));
+
+        buttonOk = findViewById(R.id.buttonOk);
+        buttonCancel = findViewById(R.id.buttonCancel);
+
+        buttonOk.setOnClickListener(onClickAction);
+        buttonCancel.setOnClickListener(onClickAction);
+    }
+
+    private final View.OnClickListener onClickAction = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (buttonOk.equals(v)) {
+                actionOk();
+            } else if (buttonCancel.equals(v)) {
+                actionCancel();
+            }
+        }
+    };
+
+    protected void actionOk() {
+        final EditText editPrice = findViewById(R.id.editPrice);
+        line.Price = Double.parseDouble(editPrice.getText().toString());
+
+        final EditText editAmountDoc = findViewById(R.id.editAmountDoc);
+        line.DocQnty = Double.parseDouble(editAmountDoc.getText().toString());
+
+        final EditText editAmountFact = findViewById(R.id.editAmountFact);
+        line.FactQnty = Double.parseDouble(editAmountFact.getText().toString());
+
+        finish();
+    }
+
+    protected void actionCancel() {
+        finish();
     }
 }
